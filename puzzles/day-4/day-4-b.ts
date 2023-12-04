05165da2cf48d4a2cdd622be5095047c271d79e6
Gergely Eory <24212611+GergelyEory@users.noreply.github.com>
2023-12-04 21:15:27 +0000

    4b

--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -2,8 +2,27 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day4b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = (await readData(dataPath)).filter((d) => d.trim().length);
+
+  const regex = /\d+/g;
+
+  const nums = data.map((line) =>
+    line
+      .split(':')[1]
+      .split('|')
+      .map((part) => part.trim().match(regex).map(Number)),
+  );
+
+  const values = nums.map((line) => line[1].filter((v) => line[0].includes(v)).length);
+  const copies: number[] = new Array(values.length).fill(1);
+
+  for (let card = 0; card < values.length; card++) {
+    for (let val = 1; val <= values[card]; val++) {
+      copies[card + val] += copies[card];
+    }
+  }
+
+  return copies.reduce((acc, curr) => acc + curr);
 }
 
 const answer = await day4b();
